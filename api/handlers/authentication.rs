3fd053692f03c54398d7f419c7705ffe71eab8dc
zanadoman <domanseven@gmail.com>
2025-10-09 08:18:52 +0200

    initial commit

---
+++ api/handlers/authentication.rs
@@ -0,0 +1,42 @@
+use axum_login::{
+    AuthSession,
+    axum::{Json, extract::State, http::StatusCode, response::IntoResponse},
+    tracing::Level,
+};
+
+use crate::{AppState, Authenticator, Credentials, HandlerError, models::User};
+
+#[axum::debug_handler]
+#[tracing::instrument(level = Level::DEBUG, skip(state))]
+pub async fn register(
+    State(state): State<AppState>,
+    Json(mut user): Json<User>,
+) -> Result<impl IntoResponse, HandlerError> {
+    let mut tx = state.pool().begin().await?;
+    user.create(&mut tx).await?;
+    tx.commit().await?;
+    Ok((StatusCode::CREATED, Json(user)))
+}
+
+#[axum::debug_handler]
+#[tracing::instrument(level = Level::DEBUG, skip(authenticator))]
+pub async fn login(
+    mut authenticator: AuthSession<Authenticator>,
+    Json(creds): Json<Credentials>,
+) -> Result<impl IntoResponse, HandlerError> {
+    let user = authenticator
+        .authenticate(creds)
+        .await?
+        .ok_or(StatusCode::UNAUTHORIZED)?;
+    authenticator.login(&user).await?;
+    Ok((StatusCode::OK, Json(user)))
+}
+
+#[axum::debug_handler]
+#[tracing::instrument(level = Level::DEBUG, skip(authenticator))]
+pub async fn logout(
+    mut authenticator: AuthSession<Authenticator>,
+) -> Result<impl IntoResponse, HandlerError> {
+    authenticator.logout().await?;
+    Ok(StatusCode::NO_CONTENT)
+}
